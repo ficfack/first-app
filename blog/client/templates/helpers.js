7d31628ab416f064c20f18fb00232db7238053ac
ficfack <thebanjoe@ficfack.com>
2017-08-24 17:25:24 +0900

    New Project blog by Haegu-Blog Project Chapter 9 ing (https://www.youtube.com/watch?v=Rl7JrhXZ7zs&list=PLskZP4iE0XzaOfpScGKmw011f4PKOIimj&index=7)

--- blog/client/templates/helpers.js
+++ blog/client/templates/helpers.js
@@ -37,3 +37,7 @@ Template.layout.events({
         return false;
     }
 });
+
+Template.registerHelper('formatDate', function (date) {
+    return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+});
